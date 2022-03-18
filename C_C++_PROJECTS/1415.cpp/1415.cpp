ab75237aacd0b6cefa431be9d0f4bec607cdc033
Jin-uu <jinu200131@gmail.com>
2022-03-18 19:19:26 +0900

    1415 사탕 풀이 완료

--- C_C++_PROJECTS/1415.cpp/1415.cpp
+++ C_C++_PROJECTS/1415.cpp/1415.cpp
@@ -1,5 +1,6 @@
 // #define DEBUG
 
+
 #include <iostream>
 #include <map>
 
@@ -12,7 +13,7 @@ bool isPrime[MAX_N * MAX_COST +1];                  // isPrime[n] = n의 소수 
 long long dp[MAX_N * MAX_COST +1] = {0, };          // dp[k] = k를 만들 수 있는 합의 경우의 수
 map<int,int> cost;                                  // cost[n] = 가격이 n인 사탕의 개수
 int candy[MAX_N+1] ={0,};                           // candy[n] = n번째로 입력받은 사탕 가격
-bool isVisited[MAX_N+1] = {false, };                // 해당 가격의 사탕에 대해 계산 수행 했는지
+bool isVisited[MAX_COST+1] = {false, };             // 해당 가격의 사탕에 대해 계산 수행 했는지
 
 void initPrime(){
     for(long long i=0; i<MAX_N * MAX_COST +1; i++){
@@ -20,7 +21,7 @@ void initPrime(){
     }
 }
 
-void getPrime(long long max){             // 1~max 까지의 소수 구하기
+void getPrime(long long max){               // 1~max 까지의 소수 구하기
     initPrime();
     for(long long i=2; i<=max; i++){
         if(isPrime[i]){
@@ -31,7 +32,7 @@ void getPrime(long long max){             // 1~max 까지의 소수 구하기
     }
 }
 
-void printPrime(long long max){
+void printPrime(long long max){             // debug
     long long cnt=0;
     cout << "prime nums : " << endl;
     for(long long i=2; i<=max; i++){
@@ -43,7 +44,7 @@ void printPrime(long long max){
     cout << endl << "total prime num's num : " << cnt << endl;
 }
 
-void printDp(long long max){
+void printDp(long long max){                // debug
     cout << "dp : ";
     for(long long i=1; i<=max; i++){
         cout << dp[i] << " ";
@@ -60,15 +61,18 @@ int main(void){
     long long total_sum=0;          // 사탕 가격의 총합
 
     for(int i=1; i<=n; i++){        // 사탕 n개의 가격 입력
-        cin >> temp_cost;           // i번째 사탕의 가격 입력받음
+        // cin >> temp_cost;           // i번째 사탕의 가격 입력받음
+        scanf("%d", &temp_cost);    // i번째 사탕의 가격 입력받음
         cost[temp_cost]++;          // 입력받은 가격의 사탕개수 +1
         candy[i] = temp_cost;       // i번째 사탕의 가격 삽입
         total_sum += temp_cost;     // 합계에 추가
     }
 
+    // cin.clear();
+
     #ifdef DEBUG
     // cout << "n : " << n << endl;
-    // printf("n : %d\n", n);
+    printf("n : %d", n);
     // cout << "candy[] : " ;
     // for(int i=1; i<=n; i++){
     //     cout << candy[i] << " ";
@@ -89,9 +93,9 @@ int main(void){
         if(isVisited[curr_cost]) continue;  // 이미 계산된 가격의 사탕이면 건너뜀
         isVisited[curr_cost] = true;
         
-        for(long long j=total_sum; j>=1; j--){            // total_sum부터 1까지에 대해
-            for(int c=1; c<=cost[curr_cost]; c++){        // 동일 가격의 사탕들 일괄 계산
-                if(j < curr_cost*c) break;
+        for(long long j=total_sum; j>=1; j--){              // total_sum부터 1까지에 대해
+            for(int c=1; c<=cost[curr_cost]; c++){          // 동일 가격의 사탕들 일괄 계산
+                if(j < curr_cost*c) break;                  // 현재 사탕의 가격 * 개수가 총 금액보다 큰 경우 break
                 dp[j] += dp[j-curr_cost*c];
             }
         }
@@ -110,7 +114,7 @@ int main(void){
 
     #ifdef DEBUG
     cout << endl << "ans : ";
-    #endif    
+    #endif
 
-    cout << ans;
+    cout << ans << endl;
 }
