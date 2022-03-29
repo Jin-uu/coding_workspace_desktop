9c3ec5fb66c62a1e77be09a9f23d439cc25da4dd
Jin-uu <jinu200131@gmail.com>
2022-03-29 21:36:57 +0900

    JAVA 파일 위치 변경

--- JAVA_PROJECTS/PL/Hello.java
+++ JAVA_PROJECTS/PL/Hello.java
@@ -1,7 +1,7 @@
-package HELLOWORLD;
+package PL;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello world??");
     }
 }
