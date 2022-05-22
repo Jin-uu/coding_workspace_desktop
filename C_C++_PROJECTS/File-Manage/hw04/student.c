2bada8c4f659aae06d8836477ae23d3df288e281
Jinu <jinu200131@gmail.com>
2022-05-22 13:55:59 +0900

    FM hw04 complete

--- C_C++_PROJECTS/File-Manage/hw04/student.c
+++ C_C++_PROJECTS/File-Manage/hw04/student.c
@@ -27,41 +27,51 @@ int readRecord(FILE *fp, STUDENT *s, int rrn){
 	fseek(fp, HEADER_SIZE + RECORD_SIZE*rrn, SEEK_SET);
 	fread(buf, RECORD_SIZE, 1, fp);
 	buf[RECORD_SIZE] = '\0';
-	printf("read: %s", buf);
-	// unpack(buf, s);
+	unpack(buf, s);
+	free(buf);
 }
 
 void unpack(const char *recordbuf, STUDENT *s){
 	char ch;
 	int idx=0;
-	int field_idx=0, field_num=1;
+	int field_idx=0, field_num=0;
 	while(idx < RECORD_SIZE){
 		switch(field_num){
 			case ID:
+				// printf("id <- %c\n",recordbuf[idx]);
 				s -> id[field_idx] = recordbuf[idx];
+				s -> id[field_idx+1] = '\0';
 				break;
 			
 			case NAME:
+				// printf("name <- %c\n",recordbuf[idx]);
 				s -> name[field_idx] = recordbuf[idx];
+				s -> name[field_idx+1] = '\0';
 				break;
 			
 			case DEPT:
+				// printf("dept <- %c\n",recordbuf[idx]);
 				s -> dept[field_idx] = recordbuf[idx];
+				s -> dept[field_idx+1] = '\0';
 				break;
 			
 			case ADDR:
+				// printf("addr <- %c\n",recordbuf[idx]);
 				s -> addr[field_idx] = recordbuf[idx];
+				s -> addr[field_idx+1] = '\0';
 				break;
 			
 			case EMAIL:
+				// printf("email <- %c\n",recordbuf[idx]);
 				s -> email[field_idx] = recordbuf[idx];
-				break;
-			
+				s -> email[field_idx+1] = '\0';
+				break;			
 		}
 		field_idx++;
 		idx++;
 		if(recordbuf[idx] == '#') {
 			field_num++;
+			idx++;
 			field_idx=0;
 		}
 	}
@@ -70,11 +80,12 @@ int writeRecord(FILE *fp, const STUDENT *s, int rrn){
 	char* buf;
 	buf = malloc(sizeof(char)*RECORD_SIZE);
 	pack(buf, s);
-	printf("rrn : %d\n", rrn);
-	printf("buf : %s\n", buf);
+	// printf("rrn : %d\n", rrn);
+	// printf("buf : %s\n", buf);
 	
 	fseek(fp, HEADER_SIZE+RECORD_SIZE*rrn, SEEK_SET);
 	fwrite(buf, RECORD_SIZE, 1, fp);
+	free(buf);
 }
 
 void pack(char *recordbuf, const STUDENT *s){
@@ -97,7 +108,7 @@ int appendRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *e
 	int file_size=-1;								// 파일 사이즈 검사
 	fseek(fp,0,SEEK_END);
 	file_size = ftell(fp);
-	printf("file_size : %d\n", file_size);
+	// printf("file_size : %d\n", file_size);
 	if(file_size <= 0){								// 빈파일이면
 		fseek(fp, 0, SEEK_SET);
 		int header = 0;							// 헤더 추가
@@ -125,8 +136,87 @@ int appendRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *e
 // 검색 결과를 출력할 때 반드시 printRecord() 함수를 사용한다. (반드시 지켜야 하며, 그렇지
 // 않는 경우 채점 프로그램에서 자동적으로 틀린 것으로 인식함)
 //
-void searchRecord(FILE *fp, enum FIELD f, char *keyval);
-void printRecord(const STUDENT *s);
+void searchRecord(FILE *fp, enum FIELD f, char *keyval){
+	fp = fopen(filename, "r+");						//파일 열기
+	if(fp == NULL){
+		printf("file open error\n");
+		exit(1);
+	}
+	int file_size=-1;								// 파일 사이즈 검사
+	fseek(fp,0,SEEK_END);
+	file_size = ftell(fp);
+	// printf("file_size : %d\n", file_size);
+	if(file_size <= 0){								// 빈파일이면
+		fseek(fp, 0, SEEK_SET);
+		int header = 0;							// 헤더 추가
+		fwrite(&header, sizeof(int), 1, fp);
+	}
+	fseek(fp,0,SEEK_SET);							// 헤더에서 저장된 레코드 개수 가져오기
+	int record_num;
+	fread(&record_num, sizeof(int), 1, fp);
+	// printf("record_num : %d\n", record_num);
+
+	for (int i = 0; i < record_num; i++) {
+		STUDENT* read_s = (STUDENT*)malloc(sizeof(char)*RECORD_SIZE);
+		readRecord(fp,read_s,i);
+		// debug
+		// printf("read student %d : ",i);
+		// printRecord(read_s);
+		//
+		switch(f){
+			case ID:
+				// printf("compairing (%s)==(%s)\n",read_s -> id, keyval);
+				if(strcmp((read_s -> id), keyval) == 0){
+					//debug
+					// printf("일치(%d)) --> ",f);
+					//
+					printRecord(read_s);
+				}
+				break;
+			case NAME:
+				// printf("compairing (%s)==(%s)\n",read_s -> name, keyval);
+				if(strcmp((read_s -> name), keyval) == 0){
+					//debug
+					// printf("일치(%d)) --> ",f);
+					//
+					printRecord(read_s);
+				}
+				break;
+			case DEPT:
+				// printf("compairing (%s)==(%s)\n",read_s -> dept, keyval);
+				if(strcmp((read_s -> dept), keyval) == 0){
+					//debug
+					// printf("일치(%d)) --> ",f);
+					//
+					printRecord(read_s);
+				}
+				break;
+			case ADDR:
+				// printf("compairing (%s)==(%s)\n",read_s -> addr, keyval);
+				if(strcmp((read_s -> addr), keyval) == 0){
+					//debug
+					// printf("일치(%d)) --> ",f);
+					//
+					printRecord(read_s);
+				}
+				break;
+			case EMAIL:
+				// printf("compairing (%s)==(%s)\n",read_s -> email, keyval);
+				if(strcmp((read_s -> email), keyval) == 0){
+					//debug
+					// printf("일치(%d)) --> ",f);
+					//
+					printRecord(read_s);
+				}
+				break;
+		}
+		free(read_s);
+	}
+	
+}
+void printRecord(const STUDENT *s){
+	printf("%s | %s | %s | %s | %s\n", s->id, s->name, s->dept, s->addr, s->email);
+}
 
 //
 // 레코드의 필드명을 enum FIELD 타입의 값으로 변환시켜 준다.
@@ -134,19 +224,19 @@ void printRecord(const STUDENT *s);
 // 프로그램 내부에서 이를 NAME(=1)으로 변환할 필요성이 있으며, 이때 이 함수를 이용한다.
 //
 enum FIELD getFieldID(char *fieldname){
-	if(fieldname == "ID"){
+	if(strcmp(fieldname,"ID") == 0){
 		return ID;
 	}
-	else if(fieldname == "NAME"){
+	else if(strcmp(fieldname,"NAME") == 0){
 		return NAME;
 	}
-	else if(fieldname == "DEPT"){
+	else if(strcmp(fieldname,"DEPT") == 0){
 		return DEPT;
 	}
-	else if(fieldname == "ADDR"){
+	else if(strcmp(fieldname,"ADDR") == 0){
 		return ADDR;
 	}
-	else if(fieldname == "EMAIL"){
+	else if(strcmp(fieldname,"EMAIL") == 0){
 		return EMAIL;
 	}
 	else{
@@ -163,11 +253,23 @@ void main(int argc, char *argv[])
 	}
 	else{
 		STUDENT temp_s;
-		readRecord(fp,&temp_s,0);
+		char *ptr = strtok(argv[2],"=");
+		// printf("prt len : %d\n", strlen(ptr));
+		char *target_field = (char*) malloc(sizeof(char)*strlen(ptr));
+		sprintf(target_field, "%s", ptr);
+		// printf("target field : (%s)\n", target_field);          // 자른 문자열 출력
+
+		ptr = strtok(NULL, " ");      // 다음 문자열을 잘라서 포인터를 반환
+		// printf("prt len : %d\n", strlen(ptr));
+		char *target_key = (char*) malloc(sizeof(char)*strlen(ptr));
+		sprintf(target_key, "%s", ptr);
+		// printf("target key : (%s)\n", target_key);          // 자른 문자열 출력
+
+		searchRecord(fp, getFieldID(target_field), target_key);
+
+		free(target_field);
+		free(target_key);
 	}
 }
 
-void printRecord(const STUDENT *s)
-{
-	printf("%s | %s | %s | %s | %s\n", s->id, s->name, s->dept, s->addr, s->email);
-}
+
