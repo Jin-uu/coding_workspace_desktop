28d877ba278a8c5cad67cdfc1cfe7bd0b9821574
Jinu <jinu200131@gmail.com>
2022-06-21 14:40:11 +0900

    beak 1330 complete

---
+++ C_C++_PROJECTS/algo/1330/1330.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int A,B;
+    cin >> A >> B;
+
+    if(A > B){ cout << ">" << endl; }
+    else if(A < B) { cout << "<" << endl; }
+    else cout << "==" << endl;
+
+    return 0;
+}
