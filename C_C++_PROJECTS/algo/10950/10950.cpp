f2ba86572a56d48da38804b1ea39cd79af04b2b9
Jinu <jinu200131@gmail.com>
2022-06-21 18:02:19 +0900

    beak 10950 complete

---
+++ C_C++_PROJECTS/algo/10950/10950.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int x;
+    cin >> x;
+
+    int a,b;
+    for(int i=0; i<x; i++){
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    
+
+    return 0;
+}
