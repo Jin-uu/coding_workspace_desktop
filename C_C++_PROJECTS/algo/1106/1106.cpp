0285ea4b418afbd29688925165c9b2f5e05e6f11
Jinu <jinu200131@gmail.com>
2022-07-25 20:35:01 +0900

    beak 1106 refactor

--- C_C++_PROJECTS/algo/1106/1106.cpp
+++ C_C++_PROJECTS/algo/1106/1106.cpp
@@ -5,25 +5,22 @@
 
 #define MAX_COST 100'000
 
-// typedef pair<int,int> pii;
-
 using namespace std;
 
 int c, n;
 vector<pair<int,int>> input;
-int dp_tbl[21][1001];       // dp[i][j] : j번째 까지 사용했을 때 고객을 i명 늘리기 위한 투자금 최소값
+int dp_tbl[21][1001];
 
 int solve(int client, int curr){
-    if(curr == 0){
-        return dp_tbl[curr][client] = ((client-1)/input[curr].second +1) * input[curr].first;
-    }
-    if(client == 0) return 0;
-    if(client < 0) return 0;
-    // cout << client << ", " << curr << endl;
+    if(client <= 0) return 0;
+    if(curr == 0) return dp_tbl[curr][client] = ((client-1)/input[curr].second +1) * input[curr].first;
+    
+
     int &ret = dp_tbl[curr][client];
     if(ret != -1) return ret;
 
-    return ret = min(solve(client - input[curr].second, curr)+input[curr].first, solve(client,curr-1));
+    int curr_cost = input[curr].first, curr_client = input[curr].second;
+    return ret = min(solve(client - curr_client, curr)+curr_cost, solve(client,curr-1));
 }
 
 int main(void){
@@ -35,16 +32,5 @@ int main(void){
     }
     
     memset(dp_tbl, -1, sizeof(dp_tbl));
-    cout << solve(c, n-1) << endl;
-
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j <= c; j++){
-    //         cout << dp_tbl[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    
+    cout << solve(c, n-1) << endl;    
 }
-
-
-
